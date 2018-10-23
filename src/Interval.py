cdfdfbe96740d961f0bf50e5569a34ee83a6ca3c
Yann Garbé <yanngarbe@gmail.com>
2018-10-23 23:55:13 +0200

    Modified storage structure for paf and mhap

--- src/Interval.py
+++ src/Interval.py
@@ -1,9 +1,9 @@
 
 """This class store the overlap informations between two reads, A and B"""
-class Inverval :
+class Interval :
 
-    def __init__(self, filename, id_A, id_B, reverse_A, 
-    reverse_B, start_A, start_B, end_A, end_B):
+    def __init__(self, filename, id_A, id_B, 
+    start_A, start_B, end_A, end_B):
         """Initiate the class with the given informations
         
         
@@ -11,8 +11,6 @@ class Inverval :
             filename : the name of the file
             id_A : the id of the Read A
             id_B : the id of the Read B
-            reverse_A : False if the Read A sequence isn't reversed, True if it is
-            reverse_B : False if the Read B sequence isn't reversed, True if it is
             start_A : the start index of the Read A overlapped sequence
             start_B : the start index of the Read B overlapped sequence
             end_A : the end index of the Read A overlapped sequence
@@ -22,9 +20,6 @@ class Inverval :
 
         self.id_A = id_A
         self.id_B = id_B
-        
-        self.reverse_A = reverse_A
-        self.reverse_B = reverse_B
         
         self.start_A = start_A
         self.start_B = start_B
@@ -47,14 +42,6 @@ class Inverval :
 
     #---------------------------
 
-    def getReverse_A(self):
-        return self.reverse_A
-
-    def getReverse_B(self):
-        return self.reverse_B
-
-    #---------------------------
-
     def getStart_A(self):
         return self.start_A
     
@@ -68,5 +55,12 @@ class Inverval :
     
     def getEnd_B(self):
         return self.end_B
+
+    #==============================================================
+
+    def toStringInterval(self):
+        return ""+self.filename+": [id_A] > "+self.id_A+"| [id_B] > "+self.id_B+ \
+        "| [Start_A] > "+self.start_A+"| [Start_B] > "+self.start_B+ \
+        "| [End_A] > "+self.end_A+"| [End_B] > "+self.end_B
 
     
--- src/ReadFiles.py
+++ src/ReadFiles.py
@@ -1,3 +1,5 @@
+import csv
+from Interval import Interval
 
 class ReadFiles:
 
@@ -77,7 +79,41 @@ class ReadFiles:
         Attributes:
             filepath : the file's path
         """
-        pass
+        dictData = {}
+
+        filename = filepath.split("/")[-1]
+
+        with open(filepath, "r") as f:
+            reader = csv.reader(f, delimiter="\t")
+            for i, line in enumerate(reader):
+                """
+                    id_A >      line[0] 
+                    id_B >      line[5]
+                    start_A >   line[2]
+                    start_B >   line[7]
+                    end_A >     line[3]
+                    end_B >     line[8]
+                """
+                tmp_interval = Interval(filename, line[0], line[5], line[2], line[7], 
+                line[3], line[8])
+                #print('line[{}] = {}'.format(i, line))
+
+
+                
+                if line[0] not in dictData:
+                    dictData[line[0]] = {} 
+                if line[5] not in dictData:
+                    dictData[line[5]] = {} 
+                
+                #To fix?
+                dictData[line[0]][line[5]] = tmp_interval
+                dictData[line[5]][line[0]] = tmp_interval
+                #print('line[{}] = {}'.format(i, line))
+
+            print(dictData['2']['77893'].toStringInterval())
+            print(dictData['77893']['2'].toStringInterval())
+            
+            return dictData
     
     def ExtractMhapData(self, filepath):
         """Extract the right informations in a mhap file and store it in an object
@@ -85,5 +121,35 @@ class ReadFiles:
         Attributes:
             filepath : the file's path
         """
-        pass
-    
+        dictData = {}
+
+        filename = filepath.split("/")[-1]
+
+        with open(filepath, "r") as f:
+            reader = csv.reader(f, delimiter=" ")
+            for i, line in enumerate(reader):
+                """
+                    id_A >      line[0] 
+                    id_B >      line[1]
+                    start_A >   line[5]
+                    start_B >   line[9]
+                    end_A >     line[6]
+                    end_B >     line[10]
+                """
+                tmp_interval = Interval(filename, line[0], line[1], 
+                line[5], line[9], line[6], line[10])
+                
+                if line[0] not in dictData:
+                    dictData[line[0]] = {} 
+                if line[1] not in dictData:
+                    dictData[line[1]] = {} 
+                
+                #To fix?
+                dictData[line[0]][line[1]] = tmp_interval
+                dictData[line[1]][line[0]] = tmp_interval
+                #print('line[{}] = {}'.format(i, line))
+
+            print(dictData['11']['4182'].toStringInterval())
+            print(dictData['4182']['11'].toStringInterval())
+            
+            return dictData
